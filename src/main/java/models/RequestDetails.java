4411c66a337dbdc599b5900a27c0aecd37b1df7f
Toby Peel <tobypl407@googlemail.com>
2020-03-03 13:36:41 +0000

    fleshing out the device class

--- src/main/java/models/RequestDetails.java
+++ src/main/java/models/RequestDetails.java
@@ -1,4 +1,6 @@
 package models;
+import misc.Device;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -9,6 +11,56 @@ import java.time.LocalTime;
 public class RequestDetails {
     @Id
     private int id;
-    LocalDate date;
-    LocalTime time;
+    private LocalDate date;
+    private LocalTime time;
+    private Device device;
+    private String ipAddress;
+
+    public RequestDetails(int id, LocalDate date, LocalTime time, Device device, String ipAddress) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.device = device;
+        this.ipAddress = ipAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 }
