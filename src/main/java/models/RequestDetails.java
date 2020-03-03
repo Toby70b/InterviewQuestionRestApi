ec4ed549ae09f1a7a4580f038c6edd75fa557bfe
Toby Peel <tobypl407@googlemail.com>
2020-03-03 13:26:33 +0000

    Intital commit

--- src/main/java/models/RequestDetails.java
+++ src/main/java/models/RequestDetails.java
@@ -1,4 +1,14 @@
 package models;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
 public class RequestDetails {
+    @Id
+    private int id;
+    LocalDate date;
+    LocalTime time;
 }
